14a69594f6ead61489fe3c2f63cddd16f4f94016
Tiago Araujo de Souza <taraujodesouza@gmail.com>
2025-09-27 14:27:52 +0200

    feat: change publish plugin

--- khord/build.gradle.kts
+++ khord/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    `maven-publish`
+    id("maven-publish")
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
