b1c75c18dd0697a98566903d7c73734624b49cd5
Tiago Araujo de Souza <taraujodesouza@gmail.com>
2025-09-27 14:22:03 +0200

    feat: java change

--- khord/build.gradle.kts
+++ khord/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
